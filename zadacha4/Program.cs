79b36d1386f9d101c3b565d581dc07780ec8c887
Natali <natalia.svetenkova@gmail.com>
2022-08-06 21:09:10 +0300

    zadacha2

---
+++ zadacha4/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("число a = ");
+int numberA = int.Parse( Console.ReadLine());
+Console.Write ("число b = ");
+int numberB = int.Parse( Console.ReadLine());
+Console.Write ("число c = ");
+int numberC = int.Parse( Console.ReadLine());
+int max = numberA;
+if (numberA>max) max = numberA;
+if (numberB>max) max = numberB;
+if (numberC>max) max = numberC;
+Console.WriteLine($"максимальное число равно {max}");
+
