ba23040f7580a7a64e2ec2d796b97a2b37fadd82
Natali <natalia.svetenkova@gmail.com>
2022-08-18 23:01:32 +0300

    Seminar4

---
+++ task25/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine ("Введите число");
+int A = int.Parse(Console.ReadLine());
+Console.WriteLine ("Введите число");
+int B = int.Parse(Console.ReadLine());
+
+int GetPow()
+{
+    int pow = 1;
+   
+    for ( int i = 1; i <= B; i++)
+    {
+        pow = pow*A;
+    }
+    return pow;
+}
+Console.WriteLine(GetPow());
---
+++ task29/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine ("Введите число");
+int N = int.Parse(Console.ReadLine());
+Console.WriteLine ("Введите число");
+int A = int.Parse(Console.ReadLine());
+Console.WriteLine ("Введите число");
+int B = int.Parse(Console.ReadLine());
+void RandArrey()
+{
+    int [] arr = new int[N];
+    for( int i=0; i<N; i++)
+    {
+        arr[i] = new Random{}.Next(A,B);
+    }
+    for(int i=0; i<arr.Length; i++)
+    {
+        Console.Write("{0} ", arr[i]);
+    }
+
+}
+RandArrey();
